b630681bbe9ec4ed3c3986545e0e2d5e4481f92b
Julie Chou <chouyuan424@gmail.com>
2022-12-08 21:18:18 -0500

    feat: init State in Progress Control and Form sections

--- src/components/Main/ProgressControl/index.jsx
+++ src/components/Main/ProgressControl/index.jsx
@@ -3,20 +3,65 @@ import Style from './ProgressControl.module.css'
 import { ReactComponent as IconNext } from '../../../icons/arrow-right.svg'
 import { ReactComponent as IconPrev } from '../../../icons/arrow-left.svg'
 
-const ProgressControl = () =>{
+function Btn({type, disabled, text, iconDisabled, onClick}){
+  if(disabled) return
+
+  let btnStyle;
+  let icon;
+  let textStyle;
+
+  if(type === 'prev'){
+    btnStyle = Style.btn__prev
+    icon = <IconPrev className={Style.arrow}/>
+    textStyle = Style.btn__prev__text
+    return(
+      <button className={btnStyle} onClick={onClick}>
+       {icon}
+       <span className={textStyle}>{text}</span>
+      </button>
+  )
+  }else{
+    btnStyle = Style.btn__next
+    icon = <IconNext className={Style.arrow}/>
+    textStyle = Style.btn__next__text
+    return(
+      <button className={btnStyle} onClick={onClick}>
+        <span className={textStyle}>{text}</span>
+        {!iconDisabled && icon}
+      </button>
+    )
+  }
+
+  
+}
+
+const ProgressControl = ({step, setStep}) =>{
+
+  let prevBtnText = '上一步'
+  let nextBtnText = '下一步'
+  if(step === 3){
+    nextBtnText = '完成訂單'
+  }
+
   return(
     <div className={Style.container}>
-      {/* <!-- prev-btn --> */}
-      <button className={Style.btn__prev}>
-        <IconPrev className={Style.arrow}/>
-        <span className={Style.btn__prev__text}>上一步</span>
-      </button>
-      {/* <!-- next-btn --> */}
-      <button className={Style.btn__next}>
-        <span className={Style.btn__next__text}>下一步</span>
-        <IconNext className={Style.arrow}/>
-      </button>
-
+      <Btn 
+        type="prev"
+        disabled={step === 1}
+        text={prevBtnText}
+        onClick={() => setStep(step - 1)}
+      />
+      <Btn 
+        type="next"
+        disabled={false}
+        text={nextBtnText}
+        iconDisabled={step === 3}
+        onClick={() => {
+          if(step<3){
+            setStep(step + 1)
+          }
+        }}
+      />
     </div>
   )
 }
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,17 +1,28 @@
 
 import StepProgress from './StepProgress/index'
 import Step1 from './Step1'
-// import Step2 from './Step2'
-// import Step3 from './Step3'
+import Step2 from './Step2'
+import Step3 from './Step3'
 import Cart from './Cart'
 import ProgressControl from './ProgressControl'
 
 import Style from './Main.module.css'
 
+import { useState } from 'react'
 
 
 
 const Main = () => {
+  const [step, setStep] = useState(1)
+  let form;
+  if(step === 1){
+    form = <Step1 />
+  }else if (step === 2){
+    form = <Step2 />
+  }else{
+    form = <Step3 />
+  }
+
   return (
     <main className={Style.main}>
       <h2>結帳</h2>
@@ -19,9 +30,7 @@ const Main = () => {
         <div className={Style.left__panel}>
           <StepProgress/>
           <form className={Style.form}>
-              <Step1/>
-              {/* <Step2/> */}
-              {/* <Step3/> */}
+            {form}
           </form>
         </div>
         <div className={Style.right__panel}>
@@ -29,7 +38,7 @@ const Main = () => {
         </div>
       </div>
       <div className={`${Style.left__panel} ${Style.progressControl__wrapper}`}>
-        <ProgressControl/>
+        <ProgressControl step={step} setStep={setStep}/>
       </div>
       
     </main>
